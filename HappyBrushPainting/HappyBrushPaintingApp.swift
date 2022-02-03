38babadedd851e319fe585681fec6d636b796706
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-02-03 11:14:59 +0100

    Initial Commit

---
+++ HappyBrushPainting/HappyBrushPaintingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBrushPaintingApp.swift
+//  HappyBrushPainting
+//
+//  Created by Oleksandr Bogdanov on 03.02.22.
+//
+
+import SwiftUI
+
+@main
+struct HappyBrushPaintingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
